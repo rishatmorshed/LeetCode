97ab98f89788cfe056cb6b40f5934e7b2445b17b
Mahbub Morshed <45009800+rishatmorshed@users.noreply.github.com>
2025-07-16 20:23:37 +0600

    Update 1295. Find numbers with even number.py

--- 1295. Find numbers with even number.py
+++ 1295. Find numbers with even number.py
@@ -2,6 +2,17 @@ def CountEvenNumbers(nums):
     count = 0
     for i in range(len(nums)):
         string_digits = str(nums[i])
-        if len(string_digits) % 2 == 0:
-            count += 1
+        if len(string_digits) % 2 == 0:		# Time complexity O(n * log10(M))
+            count += 1				        # Space complexity O(1)
     return count
+
+2nd approach:
+event_count =0
+    for i in range(len(nums)):
+        digits_count = 0
+        while(nums[i]>0):
+            nums[i] = nums[i] //10		# Time complexity O(n * log10(M))
+            digits_count += 1			# Space complexity O(1)
+        if digits_count % 2 == 0:
+            event_count += 1
+    return event_count
